fb55ed20ab9e0a354713c178ebf855bbcabba363
bmulcahy <brian.p.mulcahy@erdc.dren.mil>
2024-11-07 09:17:21 -0500

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+Not Complete
+
+UBI Mapserver still needs to be completed
+
+To update all FIM services in PROD run ./update-all-ecr.sh AFTER you login in to the PROD ECR
---
+++ update-all-ecr.sh
@@ -0,0 +1,42 @@
+ #!/bin/bash
+
+# Define the target directory
+
+# directories=("./UPDATE/DEV" "./UPDATE/PROD")
+directories=("./UPDATE/PROD")
+
+# Loop through files in the target directory
+for directory in "${directories[@]}"; do
+  # Check if the target is not a directory
+  if [ ! -d "$directory" ]; then
+    exit 1
+  fi
+  environment=$(basename "$directory")
+  repo="038611608639.dkr.ecr.us-east-1.amazonaws.com"
+  if [ "$environment" == "PROD" ]; then
+    repo="648157167324.dkr.ecr.us-gov-west-1.amazonaws.com"
+  fi
+  for file in "$directory"/*; do
+    if [ -f "$file" ]; then
+        xbase=${file##*/}
+        xfilename=${xbase%.*}
+      if [ "$xfilename" == "esd-download-api" ] || [ "$xfilename" == "esd-upload-api" ]; then
+          echo "$file"
+          echo "$xfilename"
+          # echo 038611608639.dkr.ecr.us-east-1.amazonaws.com/$xfilename:release
+          echo $repo/$xfilename:prod
+          docker build -t $xfilename -f $file --no-cache .
+          docker tag $xfilename $repo/$xfilename:prod
+          docker push $repo/$xfilename:prod
+      else
+          echo "$file"
+          echo "$xfilename"
+          # echo 038611608639.dkr.ecr.us-east-1.amazonaws.com/$xfilename:release
+          echo $repo/$xfilename:release
+          docker build -t $xfilename -f $file --no-cache .
+          docker tag $xfilename $repo/$xfilename:release
+          docker push $repo/$xfilename:release
+      fi
+    fi
+  done
+done
